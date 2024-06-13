77b13580978c33a06757049a5d2c8343736d7b88
Yaqing QI <qiyaqing@bcds.org.cn>
2024-06-13 18:41:05 +0800

    Ether and Wei

---
+++ basic/contract/08_ether_wei.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// Transactions are paid with ether: 
+// one ether is equals to 10^18 wei.
+contract EtherUints {
+    uint256 public oneWei = 1 wei;
+    // 1 wei is equal to 1
+    bool public isOneWei = (oneWei == 1);
+
+    uint256 public oneGwei = 1 gwei;
+    // 1 gwei is equal to 10^9 wei
+    bool public isOneGwei = (oneGwei == 1e9);
+
+    uint256 public oneEther = 1 ether;
+    // 1 ether is equal to 10^18 wei
+    bool public isOneEther = (oneEther == 1e18);
+}
