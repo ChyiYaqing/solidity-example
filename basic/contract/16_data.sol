5cc9799ca4d1e8c78545719e283ab597fee83a23
Yaqing QI <qiyaqing@bcds.org.cn>
2024-06-24 18:37:23 +0800

    solidity function

---
+++ basic/contract/16_data.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/***
+Storage: variable is a state variable (store on blockchain)
+Memory: variable is in memory and it exists while a function is being called
+Calldata: special data location that contains function arguments
+*/
