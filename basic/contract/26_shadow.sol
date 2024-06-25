560dbc0241b0691691848d93e392f79f17a370e8
Yaqing QI <qiyaqing@bcds.org.cn>
2024-06-25 11:17:16 +0800

    shadow

---
+++ basic/contract/26_shadow.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract A {
+    string public name = "Contract A";
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+}
+
+contract C is A {
+    // This is the correct way to override inherited state variables.
+    constructor() {
+        name = "Contract C";
+    }
+}
